1c00163d218666e6108ea46214eba59cfebdabcd
mithun2595 <mithun2595@gmail.com>
2018-03-12 01:36:39 -0700

    changes to incepv3

--- models/s3dv3.py
+++ models/s3dv3.py
@@ -112,7 +112,7 @@ class SepConv(snt.AbstractModule):
         return net
 
 
-class InceptionI3d(snt.AbstractModule):
+class s3d(snt.AbstractModule):
     """Inception-v1 I3D architecture.
 
     The model is introduced in:
@@ -180,7 +180,7 @@ class InceptionI3d(snt.AbstractModule):
         if final_endpoint not in self.VALID_ENDPOINTS:
             raise ValueError('Unknown final endpoint %s' % final_endpoint)
 
-        super(InceptionI3d, self).__init__(name=name)
+        super(s3d, self).__init__(name=name)
         self._num_classes = num_classes
         self._spatial_squeeze = spatial_squeeze
         self._final_endpoint = final_endpoint
